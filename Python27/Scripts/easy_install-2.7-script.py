51d58e5490b3a8b99dd8639aed90ad71680ea15d
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-02-13 10:21:27 +0000

    Remove repository pyc files. Refs #6527
    They seem to change anyway when moved to a different location.

--- Python27/Scripts/easy_install-2.7-script.py
+++ Python27/Scripts/easy_install-2.7-script.py
@@ -1,9 +1,9 @@
-#!python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==0.6c12dev-r85381','console_scripts','easy_install-2.7'
-__requires__ = 'setuptools==0.6c12dev-r85381'
+#!C:\Users\dmn58364\Documents\Code\mantid\Code\Third_Party\lib\win64\Python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==0.6c11','console_scripts','easy_install-2.7'
+__requires__ = 'setuptools==0.6c11'
 import sys
 from pkg_resources import load_entry_point
 
 sys.exit(
-   load_entry_point('setuptools==0.6c12dev-r85381', 'console_scripts', 'easy_install-2.7')()
+   load_entry_point('setuptools==0.6c11', 'console_scripts', 'easy_install-2.7')()
 )
