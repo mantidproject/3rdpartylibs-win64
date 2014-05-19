1006eb2478434a597953d084ecb3d664c32f6bcd
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-05-19 16:52:43 +0100

    Add epics python module.
    It uses EPICS version 3.14.12.2 DLLs. The Python epics module itself
    is pure python and simply uses the EPICS DLLs that it is pointed at. This
    means that to shift EPICS versions you can simpy drop new DLLs/exes into
    the 'epics' directory.
    Refs #9488

---
+++ Python27/Lib/site-packages/epics/devices/ai.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""Epics analog input record"""
+import epics
+
+class ai(epics.Device):
+    "Simple analog input device"
+
+    attrs = ('VAL', 'EGU', 'HOPR', 'LOPR', 'PREC', 'NAME', 'DESC',
+             'DTYP', 'INP', 'LINR', 'RVAL', 'ROFF', 'EGUF', 'EGUL',
+             'AOFF', 'ASLO', 'ESLO', 'EOFF', 'SMOO', 'HIHI', 'LOLO',
+             'HIGH', 'LOW', 'HHSV', 'LLSV', 'HSV', 'LSV', 'HYST')
+
+    def __init__(self, prefix, **kwargs):
+        if prefix.endswith('.'):
+            prefix = prefix[:-1]
+        epics.Device.__init__(self, prefix, delim='.',
+                              attrs=self.attrs, **kwargs)
