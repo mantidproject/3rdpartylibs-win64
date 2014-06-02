eb6ba7e11f2bdffa9f54ca6068dd0a05a0c8e2d7
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-06-02 09:02:53 +0100

    Add Sphinx to bundle Python packages.
    This is required so that developers can build the documentation.

---
+++ Python27/Scripts/pygmentize-script.py
@@ -0,0 +1,9 @@
+#!c:\GitHub\mantid\Code\Third_Party\lib\win64\Python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Pygments==1.6','console_scripts','pygmentize'
+__requires__ = 'Pygments==1.6'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('Pygments==1.6', 'console_scripts', 'pygmentize')()
+)
