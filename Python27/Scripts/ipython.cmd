e4805229b74b64e2604d1af2d3372c524ae9fec8
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-12-22 12:15:50 +0000

    Add dependencies required for IPython notebook on Windows.
    Refs #10825

---
+++ Python27/Scripts/ipython.cmd
@@ -0,0 +1,5 @@
+:: ipython.cmd
+:: Hand-written IPython cmd wrapper
+:: Standard executables are not relocatable
+
+"%~dp0\..\python.exe" "%~dp0\ipython.py" %*
---
+++ Python27/Scripts/ipython.py
@@ -0,0 +1,6 @@
+#!c:\GitHub\mantid\Code\Third_Party\lib\win64\Python27\python.exe
+"""Terminal-based IPython entry point.
+"""
+if __name__ == '__main__':
+    from IPython import start_ipython
+    start_ipython()
